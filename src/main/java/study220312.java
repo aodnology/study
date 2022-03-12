8b1e1a95d1bcb5a72309c542200aff8e6f31855d
trap909 <aodnology@gmail.com>
2022-03-12 22:29:47 +0900

    Initial commit

---
+++ src/main/java/study220312.java
@@ -0,0 +1,40 @@
+public class study220312 {
+
+    public static void main(String[] args) {
+
+/*  public static void main(string[] args) 단축키 == psvm + tap
+* public = 접근 제한자
+* static = 공유의 의미, 따로 객체를 생성하지 않고 사용 가능, 호출없이 자동으로 할당 가능
+*          가장 먼저 main 함수를 실행 할 수 있게 해줌
+* void = 어떠한 반환값도 없다
+* (String[] args) = 매개변수 넣는 곳 main 실행시 필요한 것들이 args 변수를 통해 들어옴
+*
+ */
+        int dataAccess = 0; // 카멜 표기법, 변수에 사용
+        int DataAccess = 0; // 파스칼표기법, 클래스 이름에 사용, 전역변수에 사용
+        int data_access = 0; // 스네이크 표기법, 변수에 사용, 언더 바 특징
+
+/* 기본형 변수 4가지 type
+
+* char == 문자 / 잘 사용하지는 않지만 외워두자
+ */
+        int a = 10; // 정수
+        float b = 10.2f; // 실수
+        double d = 1.2; // 자바의 기본 실수 타입
+        String c = "캬캬!"; // String은 선언시 대문자로 작성
+
+// 상수와 리터럴
+
+        int data1 = 100; // 변수
+        final int DATA_TWO = 1000; //상수
+
+        /* 상수는 반드시 변수 값 앞에 final을 선언 해줘야함
+        *  상수 : 변수와 같지만 한번 저장하면 값을 변경할 수 없다
+        *        선언과 동시에 반드시 초기화 해야한다
+        *        상수는 초기화 후 값을 변경할 수 없다
+        *        상수의 변수 명은 반드시 대문자로 표기해야 한다 (여러단어인 경우_사용)
+        *  리터럴 : 상수의 변수 값의 명칭, 일반 변수들과 혼동을 막기위해 명칭을 달리한다.
+         */
+
+    }
+}
---
+++ src/main/java/study220312_2.java
@@ -0,0 +1,19 @@
+public class study220312_2 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        int d = b-a;
+        int e = a*b;
+        int f = b/a;
+        int g = b%a;
+
+        System.out.println("더하기 결과는 : " + c );
+        System.out.println("빼기 결과는 : " +d );
+        System.out.println("곱하기 결과는 : " +e );
+        System.out.println("나누기 결과는 : " +f );
+        System.out.println("나머지 결과는 : " +g );
+    }
+}
