4391624ee953b36a81c1b4f468e855cf06ae1cd8
trap909 <aodnology@gmail.com>
2022-04-18 00:05:06 +0900

    2020.04.18 중간고사 대비

---
+++ src/main/java/Ex07_01.java
@@ -0,0 +1,9 @@
+public class Ex07_01 {
+    public static void main(String[] args){
+        int i = 0;
+        while (i < 5){
+            System.out.println("내 위에는 조건식 내 밑에는 증감식이 무조건 들어가야된다");
+            i++;
+        }
+    }
+}
---
+++ src/main/java/Ex07_02.java
@@ -0,0 +1,12 @@
+public class Ex07_02 {
+    public static void main(String[] args) {
+        int hap = 0;
+        int i = 1;
+
+        while (i <= 10) {
+            hap += i;
+            i++;
+        }
+        System.out.println("1 부터 10까지의 합은 :"+hap);
+    }
+}
