987c0c9c36d341dccf120471e14e698a1f8795d2
trap909 <aodnology@gmail.com>
2022-05-06 21:35:32 +0900

    2020.05.06 메소드 개념

---
+++ src/main/java/method_2.java
@@ -0,0 +1,17 @@
+public class method_2 {
+    public static void numbering() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        numbering();
+        numbering();
+        numbering();
+        numbering();
+        numbering();
+    }
+}
